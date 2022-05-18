07805a96bc886ca6f2ff165017f56fdcec5c3d9d
Fedor M <muhinfa@gmail.com>
2022-05-18 10:03:23 +0300

    Решение 1 домашнего задания

--- practice_1/script.js
+++ practice_1/script.js
@@ -23,3 +23,14 @@
 
 'use strict';
 
+let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+personalMovieDB.movies[prompt("Один из последних просмотренных фильмов?", "")] = prompt("На сколько оцените его?", "");
+personalMovieDB.movies[prompt("Один из последних просмотренных фильмов?", "")] = prompt("На сколько оцените его?", "");
+console.log(personalMovieDB);
