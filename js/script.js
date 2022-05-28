f6a37834689495bc831eebc528e89a3699091e6f
Fedor M <muhinfa@gmail.com>
2022-05-28 21:40:05 +0300

    front changes

--- js/script.js
+++ js/script.js
@@ -1,29 +1,8 @@
 "use strict";
 
-const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam', 'Zlatan'];
-
-function sortStudentsByGroups(arr) {
-    arr.sort();
-
-    let outSideAmount = arr.length % 3;
-    let outSideList = [];
-
-    if (outSideAmount === 0) {
-        outSideList.push('-');
-    } else {
-        for (let i = arr.length -1; i >= arr.length - outSideAmount; i--) {
-            outSideList.push(arr[i]);
-        }
-    }
-
-    let finalArr = [];
-
-    for (let i = 0; i < arr.length - outSideAmount; i += 3) {
-        finalArr.push([arr[i], arr[i + 1], arr[i + 2]]);
-    }
-    outSideList.sort();
-    finalArr.push(`Оставшиеся студенты: ${outSideList.join(', ')}`);
-    return finalArr;
-
-}
-console.log(sortStudentsByGroups(students));
+const box = document.getElementById('box');
+console.log(box);
+const btns = document.getElementsByTagName('button')[1];
+console.log(btns);
+const circles = document.getElementsByClassName('circle');
+console.log(circles);
